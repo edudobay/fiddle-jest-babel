25c062d3212a65ea7b696335cc45805730fc85c5
Eduardo Dobay <edudobay@gmail.com>
2018-08-14 23:58:03 -0300

    Override default @babel/env settings

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
-{
-    "presets": ["@vue/app"]
+module.exports = {
+    "presets": [
+        "@vue/app",
+        ["@babel/env", {
+            loose: false,
+            ignoreBrowserslistConfig: false,
+            exclude: [
+                'es6.array.iterator',
+                'es6.promise',
+                'es7.promise.finally'
+            ]
+        }]
+    ]
 }
